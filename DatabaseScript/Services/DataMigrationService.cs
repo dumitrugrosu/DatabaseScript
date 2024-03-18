046a2d8c821e98717e1c6a8c7e23b39ebac40a3c
dumitru.grosu <dumitru.grosu@kdu.ro>
2024-03-18 15:06:32 +0200

    wip

--- DatabaseScript/Services/DataMigrationService.cs
+++ DatabaseScript/Services/DataMigrationService.cs
@@ -113,6 +113,19 @@ namespace DatabaseScript.Services
 
             foreach (var oldMovementTug in oldMovementTugs)
             {
+                if (!_destinationContext.MngAuxTugs.Any(tug => tug.Sid == oldMovementTug.IdTug))
+                {
+                    // Log or handle the case where the TUG_SID does not exist
+                    // This could involve logging an error or taking other actions as needed
+                    Console.WriteLine($"Skipping entry: TUG_SID {oldMovementTug.IdTug} does not exist in MNG_AUX_TUGS");
+                    continue; // Move to the next iteration
+                }
+
+                if (!_destinationContext.AuxManeuvers.Any(maneuver => maneuver.Sid == oldMovementTug.IdMovement))
+                {
+                    Console.WriteLine($"Skipping entry: MOVEMENT_SID {oldMovementTug.IdMovement} does not exist in AUX_MANEUVERS");
+                    continue;
+                }
                 var newTugs = new AuxTug()
                 {
                     Sid = (long)oldMovementTug.IdMovementTugs,
@@ -126,11 +139,23 @@ namespace DatabaseScript.Services
 
             _destinationContext.Database.CloseConnection();
             _destinationContext.Database.OpenConnection();
-            _destinationContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[AUX_ESTIMATED_TIMES] ON");
+            _destinationContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[AUX_ESTIMATED_TIME] ON");
+            // Ensure unique key values
             foreach (var oldEstimatedTimes in oldEstimatedTime)
             {
+                long key = oldEstimatedTimes.IdAux; // Ensure key is of type long
 
+                // Find the entity with the specified key
+                var existingEntity = _destinationContext.AuxEstimatedTimes.Find(key);
 
+                if (existingEntity != null)
+                {
+                    // If found, modify the existing entity or detach it
+                    // Example: existingEntity.SomeProperty = newValue;
+                    _destinationContext.Entry(existingEntity).State = EntityState.Detached;
+                }
+
+                // Create a new entity instance
                 var newEstimatedTime = new Entities.AuxEstimatedTime()
                 {
                     Sid = oldEstimatedTimes.IdAux,
@@ -140,14 +165,43 @@ namespace DatabaseScript.Services
                     SumMan = oldEstimatedTimes.SumMan,
                     LastRegisterTime = DateTimeOffset.FromUnixTimeSeconds(oldEstimatedTimes.LastStamp).DateTime,
                 };
+
+                // Add the new entity to the context
                 _destinationContext.AuxEstimatedTimes.Add(newEstimatedTime);
             }
+
+            // Save changes
             _destinationContext.SaveChanges();
-            _destinationContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[AUX_ESTIMATED_TIMES] OFF");
+
+            _destinationContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[AUX_ESTIMATED_TIME] OFF");
 
             _destinationContext.Database.CloseConnection();
             _destinationContext.Database.OpenConnection();
-            _destinationContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[MNG_AUX_TUG_TYPES] ON");
+            foreach (var oldMovement in oldMovements)
+            {
+                var newPilots = new Entities.AuxPilot()
+                {
+                    AuxSid = oldMovement.IdMovement,
+                    PilotSid = oldMovement.PilotField,
+                };
+                _destinationContext.AuxPilots.Add(newPilots);
+            }
+            _destinationContext.SaveChanges();
+            _destinationContext.Database.CloseConnection();
+            _destinationContext.Database.OpenConnection();
+            foreach (var oldMovement in oldMovements)
+            {
+                var newBarges = new Entities.AuxBarge()
+                {
+                    AuxSid = oldMovement.IdMovement,
+                    BargeSid = (long)oldMovement.BargeField,
+                };
+                _destinationContext.AuxBarges.Add(newBarges);
+            }
+            _destinationContext.SaveChanges();
+            _destinationContext.Database.CloseConnection();
+            _destinationContext.Database.OpenConnection();
+            _destinationContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[MNG_AUX_TUG_TYPE] ON");
             foreach (var oldTypes in oldType)
             {
                 var newType = new MngAuxTugType()
@@ -163,7 +217,7 @@ namespace DatabaseScript.Services
                 _destinationContext.MngAuxTugTypes.Add(newType);
             }
             _destinationContext.SaveChanges();
-            _destinationContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[MNG_AUX_TUG_TYPES] OFF");
+            _destinationContext.Database.ExecuteSqlRaw("SET IDENTITY_INSERT [dbo].[MNG_AUX_TUG_TYPE] OFF");
 
             _destinationContext.Database.CloseConnection();
         }
